b614265c0a2e43276ad21ce9b92981d999a8a4e7
Gabgosq25 <gabgosq25@gmail.com>
2026-07-06 03:11:36 -0300

    Change code

--- bankAccount/BankAccount.java
+++ bankAccount/BankAccount.java
@@ -3,32 +3,42 @@ package bankAccount;
 public class BankAccount {
 
     private String accountName;
-    private int accountNumber;
+    private final int accountNumber;
     private double accountBalance;
 
-    public void deposit(double amount) {
-        accountBalance += amount;
+    public BankAccount(String accountName, int accountNumber) {
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+    }
+    public BankAccount(String accountName, int accountNumber, double accountBalance) {
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+        this.accountBalance = accountBalance;
     }
 
+    public void deposit(double amount) {
+        if (amount >= 0) {
+            this.accountBalance += amount;
+        }
+
+    }
+
+    public void withdraw(double amount) {
+        this.accountBalance -= (amount + 5.00);
+    }
 
     public void setAccountName(String accountName) {
         this.accountName = accountName;
     }
 
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public void setAccountBalance(double accountBalance) {
-        this.accountBalance = accountBalance;
-    }
-
     public String getAccountName() {
         return accountName;
     }
+
     public int getAccountNumber() {
         return accountNumber;
     }
+
     public double getAccountBalance() {
         return accountBalance;
     }
